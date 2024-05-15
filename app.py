6db20ac6a391e2a118267519daf93be3db55554d
AsherShelby <2249899756@qq.com>
2024-05-15 23:06:00 +0800

    修改标题

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ def center_window(window, width, height):
 
 if __name__ == '__main__':
     window = ttk.Window()
-    window.title("Fucking网课 v2.1 -------by 林科大涉外某神秘人士")
+    window.title("Fucking网课 v2.1 -------by AsherOne")
     center_window(window, 1360, 790)
     mainWindow = MainFrame(window)
     style = ttk.Style()
